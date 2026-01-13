068b73e98186cf0499fdadcfc3d722d9f572d1a1
oscarCardozoCuidame <ocardozo@cuidame.tech>
2026-01-13 10:10:14 -0500

    Hot fix prisma url

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
