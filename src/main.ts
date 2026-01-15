7445397835e7424be4c8a42886188eb6ae499daf
oscarCardozoCuidame <ocardozo@cuidame.tech>
2026-01-15 14:56:15 -0500

    feat(Extend multer limit): Extend the limit of multer for image´s sending

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,10 @@ async function bootstrap() {
     credentials: true,
   });
 
+  app.use(bodyParser.json({ limit: '6mb' }));
+  app.use(bodyParser.urlencoded({ limit: '6mb', extended: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
