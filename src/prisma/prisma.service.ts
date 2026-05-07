9da442f6207feb97c407716e6a8bb597fc448cd5
Oscar_Cardozo <78360032+OscarCardozoDev@users.noreply.github.com>
2026-05-06 21:23:57 -0500

    Refactor PrismaService to use pg Pool for connections

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,32 +1,33 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
+extends PrismaClient
+implements OnModuleInit, OnModuleDestroy
 {
-  constructor(configService: ConfigService) {
-    const url = configService.get<string>('DATABASE_URL');
+constructor(configService: ConfigService) {
+  const url = configService.get<string>('DATABASE_URL');
 
-    if (!url) {
-      throw new Error('DATABASE_URL is not defined');
-    }
-
-    super({
-      adapter: new PrismaPg({
-        connectionString: url,
-      }),
-    });
+  if (!url) {
+    throw new Error('DATABASE_URL is not defined');
   }
 
-  async onModuleInit() {
-    await this.$connect();
-  }
+  const pool = new Pool({ connectionString: url });
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
+  super({
+    adapter: new PrismaPg(pool),
+  });
+}
+
+async onModuleInit() {
+  await this.$connect();
+}
+
+async onModuleDestroy() {
+  await this.$disconnect();
+}
 }
