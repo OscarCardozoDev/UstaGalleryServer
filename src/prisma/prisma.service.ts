34191d55e3d9d1f953e666dca91062bd635dd3a3
OscarCardozoDev <oscar.cardozo30@hotmail.com>
2026-05-06 21:20:51 -0500

    Refactor PrismaService to use Pool for database connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,32 +1,33 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { ConfigService } from '@nestjs/config';
+  import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+  import { PrismaClient } from '../generated/prisma/client';
+  import { PrismaPg } from '@prisma/adapter-pg';
+  import { Pool } from 'pg';
+  import { ConfigService } from '@nestjs/config';
 
-@Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  constructor(configService: ConfigService) {
-    const url = configService.get<string>('DATABASE_URL');
+  @Injectable()
+  export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+  {
+    constructor(configService: ConfigService) {
+      const url = configService.get<string>('DATABASE_URL');
 
-    if (!url) {
-      throw new Error('DATABASE_URL is not defined');
+      if (!url) {
+        throw new Error('DATABASE_URL is not defined');
+      }
+
+      const pool = new Pool({ connectionString: url });
+
+      super({
+        adapter: new PrismaPg(pool),
+      });
     }
 
-    super({
-      adapter: new PrismaPg({
-        connectionString: url,
-      }),
-    });
-  }
+    async onModuleInit() {
+      await this.$connect();
+    }
 
-  async onModuleInit() {
-    await this.$connect();
+    async onModuleDestroy() {
+      await this.$disconnect();
+    }
   }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
-}
